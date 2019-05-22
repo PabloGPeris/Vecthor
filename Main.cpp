f22b6bb868adc7c375918ee57797eab09decc215
PabloGPeris <pablogperis@gmail.com>
2019-05-22 21:40:52 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,65 @@
+#include "vecthor.h"
+#include <iostream>
+#include <stdlib.h>
+
+
+
+#ifndef _TEST_UNITARIO_
+#define _TEST_UNITARIO_
+#endif // !_TEST_UNITARIO_
+
+//primera vez que uso una macro para un test unitario en mi vida. No sé si lo he hecho bien
+
+
+using namespace std;
+
+int main(){
+	 
+#ifdef _TEST_UNITARIO_
+	vecthor<int> mivec(0); //se inicializa con el número 0
+
+	mivec.push_back(1);
+	mivec.push_back(2); //añadimos un par
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.pop_back(); // eliminamos el último
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.setsize(14, 5); //añadimos 5s hasta llegar a la longitud de 14
+	//si no se pone nada donde el 5, se llena según el constructor por defecto (En el caso de ints, es 0, descubierto ahora mismo)
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.setsize(10); //se reduce la longitud a 10
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.push_back(10);
+	mivec.push_back(11);
+	mivec.push_back(12); //se añaden cosas en su posición
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.erase(11); //se elimina el elemento de la posición 11, que curiosamente es el 11 antes puesto
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.insert(11, 100); //se inserta en el 11 un número 100
+	cout << mivec << "size = " << mivec.size() << endl;
+
+	mivec.erase(0); //se elimina el primero
+	cout << mivec << "size = " << mivec.size() << endl;
+
+
+	vecthor<int> minuevovechtor(mivec); //se comprueba que funciona el CC
+	cout << minuevovechtor << "size (COPIA) = " << mivec.size() << endl;
+	
+
+
+	mivec.setsize(0); //se pone con longitud 1 (no se puede poner longitud 0)
+	cout << mivec << "size = " << mivec.size() << endl;
+
+
+
+
+#endif // _TEST_UNITARIO_
+
+	system("pause");
+	return 0;
+}
---
+++ vecthor.h
@@ -0,0 +1,219 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+//es el Dios del Trueno y los Vectores, el gran VecThor
+
+using namespace std;
+
+template <typename T> class vecthor {
+private:
+	T valor;
+	vecthor* siguiente;
+
+public:
+	//Constructor y destructor
+	vecthor() : valor(), siguiente(NULL) { ; }
+	vecthor(const vecthor<T> & c); //constructor copia definido luego
+	vecthor(T valor_inicial) : valor(valor_inicial), siguiente(NULL) { ; }
+
+	virtual ~vecthor(){ //he descubierto hoy (tras el examen) para qué sirve el destructor virtual
+		if (siguiente != NULL) {
+			delete this->siguiente; //se destruye recursivamente
+			//siguiente = NULL; //¿innecesario? 
+		}
+	}
+
+	//Funciones básicas
+	size_t size();
+
+	vecthor* push_back(const T& a = T());
+	vecthor* pop_back();
+	vecthor* setsize(size_t lon, const T& a = T());
+	vecthor* erase(unsigned int pos);
+	vecthor* insert(unsigned int pos, const T& a);
+
+
+	ostream& print(ostream& ost = cout);
+
+
+
+	//Operadores
+	T& operator [] (unsigned int pos);
+
+};
+
+/*----------------------------------
+--------  C . C O P I A    ---------
+----------------------------------*/
+
+
+template <typename T> vecthor<T>::vecthor(const vecthor<T> & c) { //constructor copia definido luego
+	const vecthor* vc = &c;
+	this->valor = vc->valor;
+	vecthor* vt = this;
+	while (vc->siguiente != NULL) {
+		vc = vc->siguiente;	
+		vt->siguiente = new vecthor<T>(vc->valor);
+		vt = vt->siguiente;
+	}
+}
+
+
+
+/*----------------------------------
+-------- F U N C I O N E S ---------
+----------------------------------*/
+
+//AÑADE UN VALOR AL VECTOR AL FINAL
+template <typename T> vecthor<T> * vecthor<T>::push_back(const T& a) {
+	vecthor *vt;
+	vt = this;
+	while (vt->siguiente != NULL) { //llega hasta el final
+		vt = vt->siguiente;
+	}
+	vt->siguiente = new vecthor(a); //crea el valor
+	return vt ->siguiente;
+}
+
+//IMPRIME EL VECTOR EN UN OSTREAM
+template <typename T> ostream& vecthor<T>::print(ostream& ost) {
+	int l = size();
+	vecthor *vt;
+	vt = this;
+
+	for (int i = 0; i < l; i++) {
+		ost << vt->valor << ' ';
+		vt = vt->siguiente;
+		//ost << this->operator[] (i) << ' '; otra forma muy fea de hacerlo pero que funciona :) y que es menos rápida computacionalmente hablando
+	} 
+	return ost;
+}
+
+//DEVUELVE LA LONGITUD DEL VECTOR
+template <typename T> size_t vecthor<T>::size() {
+	vecthor* vt;
+	vt = this;
+	int i = 1; //empieza en 1
+	while (vt->siguiente != NULL) {
+		vt = vt->siguiente;
+		i++;
+	} 
+	return i;
+}
+
+//RECORTA O AUMENTA EL VECTHOR HASTA LAS DIMENSIONES NECESARIAS
+template <typename T> vecthor<T>* vecthor<T>::setsize(size_t ln, const T& a) {
+	int la = size(); //longitud actual --- ln = longitud nueva
+	vecthor* vt = this;
+	//if (ln <= 0) ln = 1; //innecesario
+	if (ln < la) { //si es menor, se va hasta la posición de la longitud pedida, y destruye lo siguiente
+		for (int i = 1; i < ln; i++) { //i empieza en 1
+			vt = vt->siguiente;
+		}
+		delete vt->siguiente;
+		vt->siguiente = NULL;
+	}
+	else if (ln > la) { //si es mayor, se crean más, tantos como diferencia haya entre lo deseado y lo actual
+		for (int i = 0; i < ln - la; i++) {
+			push_back(a);
+		}
+	}
+	return vt;
+}
+
+//ELIMINA EL ÚLTIMO
+template <typename T> vecthor<T>* vecthor<T>::pop_back() {
+	vecthor* vt;
+	vecthor* vt_anterior = this;
+	vt = this;
+	if (vt->siguiente == NULL) return vt; //no se puede eliminar el primer valor si es el último también.
+	while (vt->siguiente != NULL) {
+		vt_anterior = vt;
+		vt = vt->siguiente;
+	}
+	delete vt;
+	vt_anterior->siguiente = NULL;
+	return vt_anterior;
+}
+
+//ELIMINA EL QUE ESTÁ EN LA POSICIÓN PEDIDA
+template <typename T> vecthor<T>* vecthor<T>::erase(unsigned int pos) {
+	vecthor* vt;
+	vecthor* vt_anterior;
+	vt = this;
+	vt_anterior = this; //me daba error de compilación si no lo incializaba
+
+	if (pos >= size()) return NULL; //no puedes eliminar algo que no existe
+	if (pos == 0) { //si quieres eliminar el primero, hay que hacerlo especial
+		if (vt->siguiente == NULL) return NULL; //no puedes eliminar el primero si no hay un segundo
+		vt = vt->siguiente;
+		vt_anterior->valor = vt->valor; //la primera posición adquiere el valor de la siguiente
+		vt_anterior->siguiente = vt->siguiente; 
+		vt->siguiente = NULL; //necesario, ya que si no, elimina todo lo siguiente (véase el destructor)
+		delete vt; //se elimina la segunda posición
+		return vt_anterior;
+	} 
+	for (int i = 0; i < pos; i++) {
+		vt_anterior = vt;
+		vt = vt->siguiente;
+	}
+	vt_anterior->siguiente = vt->siguiente;
+	vt->siguiente = NULL; //necesario, ya que si no, elimina todo lo siguiente (véase el destructor)
+	delete vt;
+	return vt_anterior;
+}
+
+//INSERTA UN NUEVO VALOR EN LA POSICIÓN PEDIDA
+template <typename T> vecthor<T>* vecthor<T>::insert(unsigned int pos, const T& a) {
+	vecthor* vt;
+	vecthor* vt_anterior;
+	vt = this;
+	vt_anterior = this;
+	if (pos < 0) return NULL;
+	if (pos >= size()) return setsize(pos, a); //si pones un valor mayor, rellena hasta dicho valor
+	if (pos == 0) {
+
+	}
+	for (int i = 0; i < pos; i++) {
+		vt_anterior = vt;
+		vt = vt->siguiente;
+	}
+	vt_anterior->siguiente = new vecthor(a); //crea un valor nuevo
+	vt_anterior->siguiente->siguiente = vt; //cierra el hueco creado, enlazanco el nuevo valor con lo siguiente
+	return vt->siguiente;
+}
+
+/*----------------------------------
+------ S O B R E C A R G A ---------
+----------------------------------*/
+
+//print - VERSIÓN PARA char (sin espacios)
+ostream& vecthor<char>::print(ostream& ost) {
+	int l = size();
+	for (int i = 0; i < l; i++) {
+		ost << this->operator[] (i); //forma muy fea de hacerlo pero que funciona :)
+	}
+	return ost;
+}
+//también medio hice una versión para matrices ( vector<vector<T>> ) pero mejor eso ya lo hago bien en verano... si eso
+
+/*----------------------------------
+-------- O P E R A D O R E S -------
+----------------------------------*/
+
+//DEVUELVE (POR REFERENCIA) EL VALOR DE LA POSICIÓN [i]
+template <typename T> T& vecthor<T>::operator [] (unsigned int pos) {
+	vecthor *vt;
+	vt = this;
+	for (int i = 0; i < pos; i++) {
+		if (vt->siguiente == NULL) break;//si te pasas, devuelve el valor del último.
+		else vt = vt->siguiente;
+	}
+	return vt->valor; 
+}
+
+template <typename T> ostream&  operator << (ostream& o, vecthor<T> v) {
+	return o << v.print(o);
+}
